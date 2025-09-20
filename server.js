4df22e5e53359ce3797fd6b7925518d98b27b7d3
Chris Drit <chrisdrit@gmail.com>
2025-09-20 11:16:41 +0900

    Debug code

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ const fastify = Fastify({
   logger: true
 })
 
+// Add error handling
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught Exception:', err);
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+  process.exit(1);
+});
+
 // Declare a route
 fastify.get('/', async function handler (request, reply) {
   return { hello: 'world' }
