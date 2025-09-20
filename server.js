167cb5ee80fecba02cc2863522e43a25f4bf2f30
Chris Drit <chrisdrit@gmail.com>
2025-09-20 13:02:41 +0900

    adding an /up healthcheck endpoint

--- server.js
+++ server.js
@@ -4,33 +4,20 @@ const fastify = Fastify({
   logger: true
 })
 
-// Add error handling
-process.on('uncaughtException', (err) => {
-  console.error('----------------------------------------------- Uncaught Exception:', err);
-  process.exit(1);
-});
-
-process.on('unhandledRejection', (reason, promise) => {
-  console.error('------------------------------------- Unhandled Rejection at:', promise, 'reason:', reason);
-  process.exit(1);
-});
-
-process.on('exit', (code) => {
-  console.log(`------------------------------------- Process exiting with code: ${code}`);
-});
-
-process.on('SIGTERM', () => {
-  console.log('----------------------------------------- Received SIGTERM');
-});
-
-process.on('SIGINT', () => {
-  console.log('------------------------------------------ Received SIGINT');
-});
 
 // Declare a route
 fastify.get('/', async function handler (request, reply) {
   return { hello: 'world' }
 })
+
+fastify.get('/up', async function handler (request, reply) {
+  return { success: 'true' }
+})
+
+astify.get('/', async function handler (request, reply) {
+  return { hello: 'world' }
+})
+
 
 // Run the server!
 try {
