b9e32e275bd5a7762017db2025c7ffd73803cc45
Chris Drit <chrisdrit@gmail.com>
2025-09-23 12:47:05 +0900

    Reverting debug code now that its working

--- appsignal.cjs
+++ appsignal.cjs
@@ -1,18 +1,12 @@
 require('dotenv').config();
 const { Appsignal } = require("@appsignal/nodejs");
 
-console.log("NODE_ENV:", process.env.NODE_ENV || "not set");
-console.log("AppSignal environment will be:", process.env.NODE_ENV || "development");
-console.log("AppSignal push API key present:", !!process.env.APPSIGNAL_PUSH_API_KEY);
-
 const appsignal = new Appsignal({
   active: true,
   name: "API",
   environment: process.env.NODE_ENV || "development",
   pushApiKey: process.env.APPSIGNAL_PUSH_API_KEY,
 });
-
-console.log("AppSignal initialized for environment:", appsignal.config.environment);
 
 // Create AppSignal logger for sending raw logs
 const logger = Appsignal.logger("fastify", "info");
