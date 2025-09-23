88e4ae792b9a7255a3f93224d2155e588778eb00
Chris Drit <chrisdrit@gmail.com>
2025-09-23 12:09:59 +0900

    More AppSignal updates

--- appsignal.cjs
+++ appsignal.cjs
@@ -1,4 +1,7 @@
+require('dotenv').config();
 const { Appsignal } = require("@appsignal/nodejs");
+
+console.log("AppSignal Push API Key:", process.env.APPSIGNAL_PUSH_API_KEY ? "***present***" : "missing");
 
 new Appsignal({
   active: true,
